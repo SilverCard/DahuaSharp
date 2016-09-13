3d85327015a2271c07d4cebcbea2b4d0028268d8
SilverCard <ricardo@outlook.in>
2016-09-13 16:53:52 -0300

    Updated project name.

--- DahuaSharp/Properties/AssemblyInfo.cs
+++ DahuaSharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmallDahuaLib")]
+[assembly: AssemblyTitle("DahuaSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SmallDahuaLib")]
+[assembly: AssemblyCompany("SilverCard")]
+[assembly: AssemblyProduct("DahuaSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
